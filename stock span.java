414db4bd62cc4927ff8ca79e336fbeebe50412cd
Rajat Singh <135345654+ichirokun12@users.noreply.github.com>
2024-10-12 21:06:24 +0530

    Update stock span.java

--- stock span.java
+++ stock span.java
@@ -1,16 +1,18 @@
-class solution {
+import java.util.Stack;
+import java.util.Scanner;
+
+class Solution {
     public static int[] solve(int[] arr) {
         int[] span = new int[arr.length];
         Stack<Integer> st = new Stack<>();
         st.push(0);
         span[0] = 1;
-        for(int i = 1; i < arr.length; i++ ) {
-            while(st.size() > 0 && arr[i]> arr[st.peek()]) {
+        for (int i = 1; i < arr.length; i++) {
+            while (!st.isEmpty() && arr[i] > arr[st.peek()]) {
                 st.pop();
-
-            } 
-            if(st.size() == 0) {
-                span[i] i + 1;
+            }
+            if (st.isEmpty()) {
+                span[i] = i + 1;
             } else {
                 span[i] = i - st.peek();
             }
@@ -18,4 +20,33 @@ class solution {
         }
         return span;
     }
-} 
+}
+
+public class EH {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Input array size
+        System.out.print("Enter the size of the array: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+
+        // Input array elements
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        // Creating an object of Solution class
+        Solution s = new Solution();
+
+        // Calling the solve method and storing the result
+        int[] result = s.solve(arr);
+
+        // Output the span array
+        System.out.println("Span array:");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + " ");
+        }
+    }
+}
